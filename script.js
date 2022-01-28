4e54b6c885fa88c39c45f48d136c23d8115e38eb
Alyson <consulialyson@gmail.com>
2022-01-28 16:47:21 -0300

    Selecionar sobremesa e liberar botao de fechar pedido

--- script.js
+++ script.js
@@ -27,7 +27,9 @@ function selecionarCarne(caixa){
     const icone = caixa.querySelector('ion-icon')
     icone.classList.remove('escondido')
 
-    secaoCarne.classList.add("temCaixa")
+    secaoCarne.classList.add('temCaixa')
+    
+    fecharPedido()
 }
 
 function selecionarBebida(caixa){
@@ -53,5 +55,54 @@ function selecionarBebida(caixa){
     const icone = caixa.querySelector('ion-icon')
     icone.classList.remove('escondido')
 
-    secaoBebida.classList.add("temCaixa")
+    secaoBebida.classList.add('temCaixa')
+
+    fecharPedido()
+}
+
+function selecionarSobremesa(caixa){
+    
+    const secaoSobremesa = document.querySelector('.sobremesa')
+
+    const sobremesa1 = document.querySelector('.sobremesa .um')
+    sobremesa1.classList.remove('boxGreen')
+    const iconeSobremesa1 = sobremesa1.querySelector('ion-icon')
+    iconeSobremesa1.classList.add('escondido')
+
+    const sobremesa2 = document.querySelector('.sobremesa .dois')
+    sobremesa2.classList.remove('boxGreen')
+    iconeSobremesa2 = sobremesa2.querySelector('ion-icon')
+    iconeSobremesa2.classList.add('escondido')
+
+    const sobremesa3 = document.querySelector('.sobremesa .tres')
+    sobremesa3.classList.remove('boxGreen')
+    const iconeSobremesa3 = sobremesa3.querySelector('ion-icon')
+    iconeSobremesa3.classList.add('escondido')
+
+    caixa.classList.add('boxGreen')
+    const iconeCaixa = caixa.querySelector('ion-icon')
+    iconeCaixa.classList.remove('escondido')
+
+    secaoSobremesa.classList.add('temCaixa')
+
+    fecharPedido()
+}
+
+function fecharPedido(){
+    const secaoCarne = document.querySelector(".carne");
+    const secaoBebida = document.querySelector(".bebida");
+    const secaoSobremesa = document.querySelector('.sobremesa')
+    
+    const selecionouCarne = secaoCarne.classList.contains('temCaixa')
+    const selecionouBebida = secaoBebida.classList.contains('temCaixa')
+    const selecionouSobremesa = secaoSobremesa.classList.contains('temCaixa')
+    
+    const botao = document.querySelector('button')
+    const textoBotao = botao.querySelector('span')
+
+    if(selecionouCarne && selecionouBebida && selecionouSobremesa){
+        textoBotao.innerHTML = 'Fechar pedido'
+        botao.classList.add('fundoVerde')
+    }
+   
 }
