a088e39e3594d61ec8d7b17e239f64eb36a9ca22
Alyson <consulialyson@gmail.com>
2022-01-29 14:27:26 -0300

    melhorar o codigo JS já feito

--- script.js
+++ script.js
@@ -3,106 +3,42 @@ function wpp(){
     prompt('Endereço: ');
 }
 
-function selecionarCarne(caixa){
-    //const caixa = document.querySelector('.opcao')
-
-    const secaoCarne = document.querySelector(".carne");
-  
-    const carne1 = document.querySelector('.carne .um')
-    carne1.classList.remove('boxGreen')
-    const iconeCarne1 = carne1.querySelector('ion-icon')
-    iconeCarne1.classList.add('escondido')
-    
-    const carne2 = document.querySelector('.carne .dois')
-    carne2.classList.remove('boxGreen')
-    const iconeCarne2 = carne2.querySelector('ion-icon')
-    iconeCarne2.classList.add('escondido')
-
-    const carne3 = document.querySelector('.carne .tres')
-    carne3.classList.remove('boxGreen')
-    const iconeCarne3 = carne3.querySelector('ion-icon')
-    iconeCarne3.classList.add('escondido')
-    
-    caixa.classList.add('boxGreen')
-    const icone = caixa.querySelector('ion-icon')
-    icone.classList.remove('escondido')
-
-    secaoCarne.classList.add('temCaixa')
-    
-    fecharPedido()
+function deselecionar(classeSecao){
+    const selecionado = document.querySelector('.'+classeSecao+' .boxGreen');
+    if (selecionado !== null) {
+        selecionado.classList.remove('boxGreen');
+        const iconeSelecionado = selecionado.querySelector('ion-icon');
+        iconeSelecionado.classList.add('escondido');
+    }
 }
 
-function selecionarBebida(caixa){
-
-    const secaoBebida = document.querySelector(".bebida");
+function selecionar(clicado, classeSecao) {
+    deselecionar(classeSecao);
+    clicado.classList.add("boxGreen");
+    const icone = clicado.querySelector("ion-icon");
+    icone.classList.remove("escondido");
     
-    const bebida1 = document.querySelector('.bebida .um')
-    bebida1.classList.remove('boxGreen')
-    const iconeBebida1 = bebida1.querySelector('ion-icon')
-    iconeBebida1.classList.add('escondido')
+    const secaoSelecionada = document.querySelector('.'+classeSecao)
 
-    const bebida2 = document.querySelector('.bebida .dois')
-    bebida2.classList.remove('boxGreen')
-    const iconeBebida2 = bebida2.querySelector('ion-icon')
-    iconeBebida2.classList.add('escondido')
-
-    const bebida3 = document.querySelector('.bebida .tres')
-    bebida3.classList.remove('boxGreen')
-    const iconeBebida3 = bebida3.querySelector('ion-icon')
-    iconeBebida3.classList.add('escondido')
-    
-    caixa.classList.add('boxGreen')
-    const icone = caixa.querySelector('ion-icon')
-    icone.classList.remove('escondido')
-
-    secaoBebida.classList.add('temCaixa')
+    secaoSelecionada.classList.add('selecionou')
 
     fecharPedido()
-}
-
-function selecionarSobremesa(caixa){
-    
-    const secaoSobremesa = document.querySelector('.sobremesa')
-
-    const sobremesa1 = document.querySelector('.sobremesa .um')
-    sobremesa1.classList.remove('boxGreen')
-    const iconeSobremesa1 = sobremesa1.querySelector('ion-icon')
-    iconeSobremesa1.classList.add('escondido')
-
-    const sobremesa2 = document.querySelector('.sobremesa .dois')
-    sobremesa2.classList.remove('boxGreen')
-    iconeSobremesa2 = sobremesa2.querySelector('ion-icon')
-    iconeSobremesa2.classList.add('escondido')
-
-    const sobremesa3 = document.querySelector('.sobremesa .tres')
-    sobremesa3.classList.remove('boxGreen')
-    const iconeSobremesa3 = sobremesa3.querySelector('ion-icon')
-    iconeSobremesa3.classList.add('escondido')
-
-    caixa.classList.add('boxGreen')
-    const iconeCaixa = caixa.querySelector('ion-icon')
-    iconeCaixa.classList.remove('escondido')
-
-    secaoSobremesa.classList.add('temCaixa')
-
-    fecharPedido()
-}
+  }
 
 function fecharPedido(){
-    const secaoCarne = document.querySelector(".carne");
-    const secaoBebida = document.querySelector(".bebida");
+     
+    const secaoCarne = document.querySelector('.carne')
+    const secaoBebida = document.querySelector('.bebida')
     const secaoSobremesa = document.querySelector('.sobremesa')
-    
-    const selecionouCarne = secaoCarne.classList.contains('temCaixa')
-    const selecionouBebida = secaoBebida.classList.contains('temCaixa')
-    const selecionouSobremesa = secaoSobremesa.classList.contains('temCaixa')
-    
-    const botao = document.querySelector('button')
-    const textoBotao = botao.querySelector('span')
+
+    let selecionouCarne = secaoCarne.classList.contains('selecionou')
+    let selecionouBebida = secaoBebida.classList.contains('selecionou')
+    let selecionouSobremesa = secaoSobremesa.classList.contains('selecionou')
 
     if(selecionouCarne && selecionouBebida && selecionouSobremesa){
-        textoBotao.innerHTML = 'Fechar pedido'
+        const botao = document.querySelector('button')
+        const textoBotao = botao.querySelector('span')
         botao.classList.add('fundoVerde')
+        textoBotao.innerHTML = 'Fechar pedido'
     }
-   
 }
