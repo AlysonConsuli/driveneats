92fa8d92f01a4c32cd88a80606d1b9ff83b25cb4
Alyson <consulialyson@gmail.com>
2022-01-30 11:43:31 -0300

    Envio wpp com os dois prompt já implementados

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
-function wpp(){
-    prompt('Nome: ');
-    prompt('Endereço: ');
-}
+let carneSelecionada = null
+let bebidaSelecionada = null
+let sobremesaSelecionada = null
+let precoCarne = 0
+let precoBebida = 0
+let precoSobremesa = 0
+let total = 0
 
-function deselecionar(classeSecao){
+function selecionar(clicado, classeSecao, nome, preco) {
+    deselecionar(classeSecao);
+    clicado.classList.add("boxGreen");
+    const icone = clicado.querySelector("ion-icon");
+    icone.classList.remove("escondido");
+    
+    const secaoSelecionada = document.querySelector('.'+classeSecao)
+    secaoSelecionada.classList.add('selecionou')
+
+    if (classeSecao === 'carne'){
+        carneSelecionada = nome
+        precoCarne = preco
+    } else if(classeSecao === 'bebida'){
+        bebidaSelecionada = nome
+        precoBebida = preco
+    } else if(classeSecao === 'sobremesa'){
+        sobremesaSelecionada = nome
+        precoSobremesa = preco
+    }
+    total = (precoCarne + precoBebida + precoSobremesa).toFixed(2)
+
+    fecharPedido()
+  }
+
+  function deselecionar(classeSecao){
     const selecionado = document.querySelector('.'+classeSecao+' .boxGreen');
     if (selecionado !== null) {
         selecionado.classList.remove('boxGreen');
@@ -11,19 +38,6 @@ function deselecionar(classeSecao){
         iconeSelecionado.classList.add('escondido');
     }
 }
-
-function selecionar(clicado, classeSecao) {
-    deselecionar(classeSecao);
-    clicado.classList.add("boxGreen");
-    const icone = clicado.querySelector("ion-icon");
-    icone.classList.remove("escondido");
-    
-    const secaoSelecionada = document.querySelector('.'+classeSecao)
-
-    secaoSelecionada.classList.add('selecionou')
-
-    fecharPedido()
-  }
 
 function fecharPedido(){
      
@@ -42,3 +56,23 @@ function fecharPedido(){
         textoBotao.innerHTML = 'Fechar pedido'
     }
 }
+
+function wpp() {
+    
+    const botao = document.querySelector('button')
+    const liberado = botao.classList.contains('fundoVerde')
+    if(liberado){
+        let nomeComprador = prompt('Qual é o seu nome?')
+        let endereco = prompt('Qual o seu endereço?')
+        let msg = 'Olá, gostaria de fazer o pedido: \n- Prato: ' + 
+        carneSelecionada + '\n- Bebida: ' + 
+        bebidaSelecionada + '\n- Sobremesa: ' + 
+        sobremesaSelecionada + '\nTotal: R$ ' + total +
+        '\n\nNome: ' + nomeComprador + '\nEndereco: ' + endereco
+        window.open("https://wa.me/5522999409879?text=" + encodeURIComponent(msg));
+    }
+}
+
+/*function revisao(){
+    
+}*/
